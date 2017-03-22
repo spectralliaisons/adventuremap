128832f712a247b1669dc7005124dbc3410a1f8c
Wes Jackson <wes.jackson@brainplasticity.com>
2017-03-22 07:44:05 -0700

    use single map with navbar

--- script/gpstracker.js
+++ script/gpstracker.js
@@ -1,40 +1,26 @@
-function initTracker(place){
+function initTracker(place) {
     
 //    if (place == "RussianRiver") {
 //        $("#"+place).load("notyet.html");
 //        return;
 //    }
+    document.getElementById("loader").style.display = "block";
     
     // TODO: not this? google needs kml to be hosted from publicly-visible location
     var origin = "https://s3-us-west-1.amazonaws.com/wesmjackson.com";//"https://github.com/spectralliaisons/multimap";//; // location.origin
     
-    var basePath = origin + "/gps/Places/" + place
+    var basePath = origin + "/gps/Places/" + place;
     
     function cacheBuster(){return "?rev=" + (new Date()).getTime()};
     
-    $.getJSON(basePath + "/info.json" + cacheBuster(), function(json){
+    $.getJSON(basePath + "/info.json" + cacheBuster(), function(json) {
 
         var cen = _.findWhere(json.locations, {"label":json.center}).loc
         
         // setup map type
-//        if (!window.gmap) {
-//            window.gmap = new google.maps.Map(document.getElementById('map'), {
-//                zoom: json.zoom,
-//                center: cen,
-//                mapTypeId: json.mapType,
-//                mapTypeControlOptions : {
-//                    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-//                    mapTypeIds: ['terrain', 'hybrid']
-//                },
-//                fullscreenControl:true
-//            });
-//        }
-//        else {
-//            gmap.setZoom(json.zoom);
-//            gmap.setCenter(cen);
-//            gmap.setMapTypeId(json.mapType);
-//        }
-        var gmap = new google.maps.Map(document.getElementById("map-" + place), {
+        if (!window.gmap) {
+            console.log("new gmap for " + place);
+            window.gmap = new google.maps.Map(document.getElementById('map'), {
                 zoom: json.zoom,
                 center: cen,
                 mapTypeId: json.mapType,
@@ -44,52 +30,70 @@ function initTracker(place){
                 },
                 fullscreenControl:true
             });
-        
-        // add kml layers
-        // param to kml url prevents caching by Google
-        _.each(json.layers, function(layer) {
-            trackLayer = new google.maps.KmlLayer({
-                url: basePath + /kml/ + layer + cacheBuster(),
-                map: gmap
-            });
-        });
-        
-        // add interactive marker for each location
-        _.each(json.locations, function(location){
             
-            var imgLgSrc = basePath + /img/ + location.img + cacheBuster();
-            var imgSmSrc = basePath + /imgSm/ + location.img + cacheBuster();
-            var audSrc = basePath + /aud/ + location.aud + cacheBuster();
+            google.maps.event.addListener(window.gmap, 'idle', function(){
+                // do something only the first time the map is loaded
+                document.getElementById("loader").style.display = "none";
+            });
+        }
+        else {
+            console.log("set gmap for " + place);
+            gmap.setZoom(json.zoom);
+            gmap.setCenter(cen);
+            gmap.setMapTypeId(json.mapType);
+        }
+        
+        // only load each place once
+        if (window.maps.indexOf(place) == -1) {
             
-            // html contents of marker window
-            var contentString = '<div id="map-item-content">'+
-            "<h2>" + location.label + "</h2>"+
-            "<a href='" + imgLgSrc + "' target='_blank' id='map-item-content-img'><img src='" + imgSmSrc + "' id='map-item-content-img' ></img></a>"+
-            (location.aud ? "<audio controls style='width: 100%'><source src='" + audSrc + "' type='audio/mpeg'>Your browser does not support audio. Good job!</audio>" : "")+
-            '</p>'+
-            '</div>'+
-            '</div>';
-
-            //  only one info window at a time
-            var currInfoWindow = new google.maps.InfoWindow({
-                content: contentString
+            window.maps.push(place);
+            
+            // add kml layers
+            // param to kml url prevents caching by Google
+            _.each(json.layers, function(layer) {
+                trackLayer = new google.maps.KmlLayer({
+                    url: basePath + /kml/ + layer + cacheBuster(),
+                    map: gmap
+                });
             });
 
-            var marker = new google.maps.Marker({
-                position: location.loc,
-                map: gmap
-            });
+            // add interactive marker for each location
+            _.each(json.locations, function(location){
 
-            marker.addListener('click', function() {
-                
-                if (window.lastInfoWindow) {
-                    window.lastInfoWindow.close();
-                }
-                
-                currInfoWindow.open(gmap, marker);
-                
-                window.lastInfoWindow = currInfoWindow;
+                var imgLgSrc = basePath + /img/ + location.img + cacheBuster();
+                var imgSmSrc = basePath + /imgSm/ + location.img + cacheBuster();
+                var audSrc = basePath + /aud/ + location.aud + cacheBuster();
+
+                // html contents of marker window
+                var contentString = '<div id="map-item-content">'+
+                "<h2>" + location.label + "</h2>"+
+                "<a href='" + imgLgSrc + "' target='_blank' id='map-item-content-img'><img src='" + imgSmSrc + "' id='map-item-content-img' ></img></a>"+
+                (location.aud ? "<audio controls style='width: 100%'><source src='" + audSrc + "' type='audio/mpeg'>Your browser does not support audio. Good job!</audio>" : "")+
+                '</p>'+
+                '</div>'+
+                '</div>';
+
+                //  only one info window at a time
+                var currInfoWindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                var marker = new google.maps.Marker({
+                    position: location.loc,
+                    map: gmap
+                });
+
+                marker.addListener('click', function() {
+
+                    if (window.lastInfoWindow) {
+                        window.lastInfoWindow.close();
+                    }
+
+                    currInfoWindow.open(gmap, marker);
+
+                    window.lastInfoWindow = currInfoWindow;
+                });
             });
-        });
+        }
     });
 }
--- script/navigation.js
+++ script/navigation.js
@@ -15,10 +15,69 @@ if (!window.loadContent) {
         
     window.loadContent = function(href) {
         
-        dest = $(href)[0].id
+        console.log("loadContent " + href);
         
-        console.log(dest)
+        item = $(href)[0]
         
-        initTracker(dest)
+        console.log(item.id)
+        
+        initTracker(item.id)
+        
+        // no need to reload same page
+        if (window.currpg == item)
+            return
+        
+        // set the page title
+        $.address.title($.address.title().split(' | ')[0] + ' | ' + item.id);
+        
+        // color current link
+        window.currpg = item;
+        
+        // update navigation history
+        history.pushState({id: item.id}, item.id, href)
     }
+    
+    // The following code is based off jquery.address-1.5/samples/tabs/index.html
+    
+    var tabs,
+        tabEvent = false,
+        initialTab = 'RussianRiver',
+        navSelector = '#nav li a',
+        navFilter = function(el) {
+            return $(el).attr('href').replace(/^#/, '');
+        },
+        panelSelector = '#page',
+        panelFilter = function() {
+            $(panelSelector + ' a').filter(function() {
+                return $(navSelector + ' a[title=' + $(this).attr('title') + ']').size() != 0;
+            }).each(function(event) {
+                $(this).attr('href', '#' + $(this).attr('title').replace(/ /g, '_'));
+            });
+        };
+    
+    // Initializes plugin features
+    $.address.strict(false).wrap(true);
+    
+//    if ($.address.value() == '') {
+//        $.address.history(false).value(initialTab).history(true);
+//    }
+    
+    // enable back, fwd button history
+    // http://stackoverflow.com/questions/824349/modify-the-url-without-reloading-the-page/3354511#3354511
+    window.onpopstate = function(e){
+        if(e.state){
+            
+            window.loadContent('#' + e.state.id);
+        }
+    };
+    
+    // Address handler
+    $.address.init(function(event) {
+        
+    }).change(function(event) {
+    
+        if (event.value != "") {
+            window.loadContent('#' + event.value);
+        }
+    });
 }
