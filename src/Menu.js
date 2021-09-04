4ede026930edb3b2fb86a7865225e42d4113d48c
spectralliaisons <wes@wesmjackson.com>
2021-09-04 18:38:49 -0500

    onclick list element

--- src/Menu.js
+++ src/Menu.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Menu.scss';
 
-function Menu({places}) {
-  return (
-    <ol>
-      {places.map(({disp, id}) => (
-        <li key={id}>{disp}</li>
-      ))}
-    </ol>
-  );
+const Menu = ({places}) => {
+
+    return (
+        <ol>{places.map(({disp,id}) => <div key={id}><Item disp={disp} id={id}/></div>)}</ol>
+    );
+};
+
+
+const Item = ({disp, id}) => {
+    const [loaded, setLoaded] = useState("unloaded");
+
+    console.log(`Item disp: ${disp} id: ${id} loaded: ${loaded}`);
+
+    function fetch() {
+        setLoaded("loaded");
+    }
+
+    return (
+        <li id={id} className={loaded} onClick={fetch}>{disp}</li>
+    );
 };
 
 export default Menu;
