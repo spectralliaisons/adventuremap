077a3c9090b1ecee2000c613126aa13a6bde1f5f
spectralliaisons <wes@wesmjackson.com>
2021-09-05 13:40:40 -0500

    underscores for non-exposed fns

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,29 @@
 let _ = require('underscore');
 
-const s3rsc = where => `https://s3-us-west-2.amazonaws.com/multimap-2/gps/s3/${where}?rev=${(new Date()).getTime()}`;
+const _s3rsc = where => `https://s3-us-west-2.amazonaws.com/multimap-2/gps/s3/${where}?rev=${(new Date()).getTime()}`;
 
-let cache = {};
+let _cache = {};
 
-const fetchJSON = which =>
-  fetch(s3rsc(`${which}.json`))
+const fetchPlaces = () =>
+  _fetchJSON("all_rivers")
+    .then(({places}) => Promise.resolve(_.sortBy(places, "disp")));
+
+const loadPlace = (place, paintData) => {
+  if (_cache[place] == null) {
+    return _fetchJSON(`${place}/info`)
+      .then(json => {
+          _cache[place] = json;
+          _.each(json.layers, _loadLayer(place, paintData));
+          return Promise.resolve({json:json, paint:true});
+      });
+  }
+  else {
+    return Promise.resolve({json:_cache[place], paint:false});
+  }
+};
+
+const _fetchJSON = which =>
+  fetch(_s3rsc(`${which}.json`))
     .then(res => {
       if (res.ok) {
         return(res.json());
@@ -13,28 +31,10 @@ const fetchJSON = which =>
       else {
         return Promise.reject(`Unable to fetch ${which}.json`);
       }
-    })
+    });
 
-const loadPlace = (place, paintData) => {
-  if (cache[place] == null) {
-    return fetchJSON(`${place}/info`)
-      .then(json => {
-          cache[place] = json;
-          _.each(json.layers, loadLayer(place, paintData));
-          return Promise.resolve({json:json, paint:true});
-      });
-  }
-  else {
-    return Promise.resolve({json:cache[place], paint:false});
-  }
-}
-
-const fetchPlaces = () =>
-  fetchJSON("all_rivers")
-    .then(({places}) => Promise.resolve(_.sortBy(places, "disp")));
-
-const loadLayer = (place, paintData) => layer => 
-  fetch(s3rsc(`${place}/geojson/${layer}`))
+const _loadLayer = (place, paintData) => layer => 
+  fetch(_s3rsc(`${place}/geojson/${layer}`))
     .then((res) => res.json())
     .then(paintData(layer))
 
